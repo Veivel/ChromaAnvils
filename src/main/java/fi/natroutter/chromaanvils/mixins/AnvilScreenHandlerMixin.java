0d88928eb7bb0286946936f3403440d359e96c8b
Veivel <25278964+Veivel@users.noreply.github.com>
2025-02-25 13:15:08 +0700

    chore: update dependencies for 1.21.4 compatibility

--- src/main/java/fi/natroutter/chromaanvils/mixins/AnvilScreenHandlerMixin.java
+++ src/main/java/fi/natroutter/chromaanvils/mixins/AnvilScreenHandlerMixin.java
@@ -13,6 +13,7 @@ import net.minecraft.screen.AnvilScreenHandler;
 import net.minecraft.screen.ForgingScreenHandler;
 import net.minecraft.screen.ScreenHandlerContext;
 import net.minecraft.screen.ScreenHandlerType;
+import net.minecraft.screen.slot.ForgingSlotsManager;
 import net.minecraft.text.Text;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
@@ -28,8 +29,8 @@ public abstract class AnvilScreenHandlerMixin extends ForgingScreenHandler {
 
     @Shadow private @Nullable String newItemName;
 
-    public AnvilScreenHandlerMixin(@Nullable ScreenHandlerType<?> type, int syncId, PlayerInventory playerInventory, ScreenHandlerContext context) {
-        super(type, syncId, playerInventory, context);
+    public AnvilScreenHandlerMixin(@Nullable ScreenHandlerType<?> type, int syncId, PlayerInventory playerInventory, ScreenHandlerContext context, ForgingSlotsManager forgingSlotsManager) {
+        super(type, syncId, playerInventory, context, forgingSlotsManager);
     }
 
     @Inject(method = "updateResult", at = @At(
